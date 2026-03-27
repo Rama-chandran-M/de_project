29ff066728c57a7fe6beecf6997391c382f1baa8
Rama-chandran-M <ramachandranmuthusamy7@gmail.com>
2026-03-27 08:08:38 +0000

    final_changes

--- 01_bronze/01_py_scripts/pf_load_data_as_table.py
+++ 01_bronze/01_py_scripts/pf_load_data_as_table.py
@@ -4,26 +4,21 @@ import re
 files = dbutils.fs.ls("s3://aws-demo-bucket090/source_data")
 print(files)
 for f in files:
-    # Everything inside the loop MUST be indented
     if f.name.endswith(".csv"):
-        # 1. Clean Table Name
         table_name = (f.name.replace(".csv", "")
                             .lower()
                             .replace(" ", "_")
                             .replace("-", "_"))
         
-        # 2. Read the file into a DataFrame
         df = spark.read.format("csv") \
                   .option("header", "true") \
                   .option("inferSchema", "true") \
                   .load(f.path)
         
-        # 3. Clean Column Names
         new_column_names = [re.sub(r'[^a-z0-9]', '_', col.lower()).strip('_') for col in df.columns]
         print(new_column_names)
         df = df.toDF(*new_column_names)
         
-        # 4. Write to table
         spark.sql("create schema if not exists 01_bronze_catalog.raw_schema")
         df.write.mode("overwrite").saveAsTable(f"`01_bronze_catalog`.raw_schema.{table_name}")
         
